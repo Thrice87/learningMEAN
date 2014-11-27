0837e98147e6b6c760243e42c242b3ae5d5a56cc
Thrice87 <me@james-prescott.com>
2014-11-27 22:48:53 +0000

    Initial Commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/learningmean-dev'
+  },
+
+  seedDB: true
+};
